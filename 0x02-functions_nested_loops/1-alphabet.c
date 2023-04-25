c6926190a3619c5726cc187de7b60359f436cdd2
HajarFERDI <hajarferdi@gmail.com>
2023-04-25 05:03:10 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * prints the alphabet - in lowercase
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
