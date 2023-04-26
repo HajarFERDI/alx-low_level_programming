6837419131a62396f26a66e0573fdd4407d07466
HajarFERDI <hajarferdi@gmail.com>
2023-04-26 06:19:55 -0700

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,11 +11,16 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
 	{
 		largest = b;
 	}
